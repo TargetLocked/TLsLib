ac5cae124f20a7d4d3f9ac33c5f3b7e5234fa4a5
TargetLocked <32962687+TargetLocked@users.noreply.github.com>
2018-06-11 21:35:08 +0800

    Add files via upload

--- LeftistTree.cpp
+++ LeftistTree.cpp
@@ -26,28 +26,33 @@ struct LeftistTree {
 	inline LeftistTree() {clear();}
 	inline int newnode() {
 		int loc=q.front(); q.pop();
-		t[loc].ls=t[loc].rs=t[loc].dep=0;
-		t[loc].sz=1;
+		t[loc].ls=t[loc].rs=t[loc].dep=t[loc].sz=0;
 		return loc;
 	}
+	//merge b into a
 	int merge(int a,int b) {
 		if(a==0||b==0) return a+b;
-		if(t[a].d>t[b].d) std::swap(a,b);
+		if(t[a].d>t[b].d) std::swap(a,b); //root has the minimum value
 		t[a].rs=merge(t[a].rs,b);
 		if(t[t[a].ls].dep<t[t[a].rs].dep) std::swap(t[a].ls,t[a].rs);
 		t[a].dep=t[t[a].rs].dep+1;
-		t[a].sz=t[t[a].ls].sz+t[t[a].rs].sz;
+		t[a].sz=t[t[a].ls].sz+t[t[a].rs].sz+1;
 		return a;
 	}
 	inline void insert(const datatype& d) {
+		if(treesz==0) {
+			t[rt].d=d;
+			treesz=1;
+			return;
+		}
 		int loc=newnode();
 		t[loc].d=d;
-		if(treesz==0) rt=1; else rt=merge(rt,loc);
+		rt=merge(rt,loc);
 	}
 	inline datatype top() {return t[rt].d;}
 	inline void pop() {
-		q.push(rt);
-		rt=merge(t[rt].ls,t[rt].rs);
+		if(t[rt].ls==0&&t[rt].rs==0) t[rt].sz=0;
+		else q.push(rt),rt=merge(t[rt].ls,t[rt].rs);
 	}
 	inline void build(datatype* st,datatype* ed) {
 		std::queue<int> q; while(!q.empty()) q.pop();
@@ -69,7 +74,7 @@ struct LeftistTreeForest {
 		datatype d;
 	}t[Msize];
 	std::queue<int> q;
-	int rt[Msize],tn;
+	int rt[Msize];
 	
 	inline int& operator[](const int k) {return rt[k];}
 	inline void clear() {
@@ -79,34 +84,40 @@ struct LeftistTreeForest {
 	}
 	inline LeftistTreeForest() {clear();}
 	inline int size(int a) {return t[rt[a]].sz;}
+	inline bool empty(int a) {return t[rt[a]].sz==0;}
 	inline int newnode() {
 		int loc=q.front(); q.pop();
-		t[loc].ls=t[loc].rs=t[loc].dep=0;
-		t[loc].sz=1;
+		t[loc].ls=t[loc].rs=t[loc].dep=t[loc].sz=0;
 		return loc;
 	}
 	int auxmerge(int a,int b) {
 		if(a==0||b==0) return a+b;
-		if(t[a].d>t[b].d) std::swap(a,b);
+		if(t[a].d>t[b].d) std::swap(a,b); //root has the minimum value
 		t[a].rs=auxmerge(t[a].rs,b);
 		if(t[t[a].ls].dep<t[t[a].rs].dep) std::swap(t[a].ls,t[a].rs);
 		t[a].dep=t[t[a].rs].dep+1;
-		t[a].sz=t[t[a].ls].sz+t[t[a].rs].sz;
+		t[a].sz=t[t[a].ls].sz+t[t[a].rs].sz+1;
 		return a;
 	}
+	//merge b into a
 	inline void merge(int a,int b) {
 		rt[a]=auxmerge(rt[a],rt[b]);
 	}
 	inline void insert(int a,const datatype& d) {
+		if(t[rt[a]].sz==0) {
+			t[rt[a]].d=d;
+			t[rt[a]].sz=1;
+			return;
+		}
 		int loc=newnode();
 		t[loc].d=d;
-		if(t[rt[a]].sz==0) rt[a]=1; else rt[a]=auxmerge(rt[a],loc);
+		rt[a]=auxmerge(rt[a],loc);
 	}
 	inline datatype top(int a) {return t[rt[a]].d;}
 	inline void pop(int a) {
-		q.push(rt[a]);
-		rt[a]=auxmerge(t[rt[a]].ls,t[rt[a]].rs);
+		if(t[rt[a]].ls==0&&t[rt[a]].rs==0) t[rt[a]].sz=0;
+		else q.push(rt[a]),rt[a]=auxmerge(t[rt[a]].ls,t[rt[a]].rs);
 	}
-	inline void newtree(int a=-1) {if(a==-1) a=++tn; rt[a]=newnode();}
+	inline void newtree(int a) {rt[a]=newnode();}
 };
 
---
+++ _References - Union Find Set.cpp
@@ -0,0 +1,14 @@
+
+namespace ufs {
+	int fa[N],rk[N];
+	int Find(int u) {return fa[u]==0 ? u : fa[u]=Find(fa[u]);}
+	inline void Union(int u,int v) {
+		u=Find(u),v=Find(v);
+		if(rk[u]<=rk[v]) {
+			fa[u]=v;
+			if(rk[u]==rk[v]) ++rk[v];
+		}
+		else fa[v]=u;
+	}
+	inline void clear() {memset(fa,0,sizeof(fa)),memset(rk,0,sizeof(rk));}
+}
