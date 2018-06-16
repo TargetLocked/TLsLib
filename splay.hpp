c1aa5315b6df2eae7b04cae97f4431e21f0a7f14
TargetLocked <32962687+TargetLocked@users.noreply.github.com>
2018-06-16 17:47:49 +0800

    Add files via upload

--- splay.hpp
+++ splay.hpp
@@ -2,13 +2,13 @@
 TLsLib - version WIP 0.1
 template struct Splay
 */
-#include <algorithm>
 #include <stack>
 #include <cstring>
+#include <cstdlib>
 
 template <class datatype>
 struct Splay {
-	static const int Msize=1e5+5;
+	static const size_t Msize=1e5+5;
 	std::stack<int> p;
 	struct spnode {
 		int fa,sz,cnt,son[2];
@@ -20,6 +20,7 @@ struct Splay {
 	
 	inline void clear() {
 		rt=0; t[0].clear();
+		while(!p.empty()) p.pop();
 		for(int i=1;i<Msize;++i) p.push(i);
 	}
 	inline Splay() {clear();}
@@ -29,10 +30,7 @@ struct Splay {
 		return ret;
 	}
 	inline int sonid(int x) {return t[t[x].fa].son[1]==x;}
-	inline void updnode(int x) {
-		if(x==0) return;
-		t[x].sz=t[t[x].son[0]].sz+t[t[x].son[1]].sz+t[x].cnt;
-	}
+	inline void updnode(int x) {t[x].sz=t[t[x].son[0]].sz+t[t[x].son[1]].sz+t[x].cnt;}
 	inline void rotate(int x) {
 		int f=t[x].fa,id=sonid(x);
 		if(t[f].fa) t[t[f].fa].son[sonid(f)]=x;
@@ -49,29 +47,99 @@ struct Splay {
 		}
 		if(T==0) rt=x;
 	}
-	inline void insert(const datatype& d) {
+	inline int insert(const datatype& d) {
 		if(rt==0) {
-			rt=1,t[1].d=d;
-			return;
+			rt=newnode(0),t[rt].cnt=t[rt].sz=1,t[rt].d=d;
+			return 1;
 		}
 		int x=rt,f=0;
 		while(1) {
 			if(t[x].d==d) {
 				++t[x].cnt;
 				updnode(x),updnode(f),splay(x);
-				return;
+				return x;
 			}
 			f=x,x=t[f].son[d>t[f].d];
 			if(x==0) {
 				int y=(t[f].son[d>t[f].d]=newnode(f));
 				t[y].d=d;
 				updnode(f),splay(y);
-				return;
+				return y;
 			}
 		}
+	}
+	inline int getrank(const datatype& d) {
+		int x=rt,ret=0;
+		while(t[x].d!=d) {
+			if(d>t[x].d) ret+=t[t[x].son[0]].sz+t[x].cnt;
+			x=t[x].son[d>t[x].d];
+		}
+		splay(x);
+		return ret+1;
+	}
+	inline datatype findrank(int r) {
+		int x=rt,tmp=t[t[x].son[0]].sz+t[x].cnt;
+		while(1) {
+			if(r<=t[t[x].son[0]].sz) x=t[x].son[0];
+			else {
+				tmp=t[t[x].son[0]].sz+t[x].cnt;
+				if(r<=tmp) break;
+				r-=tmp,x=t[x].son[1];
+			}
+		}
+		splay(x);
+		return t[x].d;
+	}
+	//iterator operations : find , find previous , find next
+	inline int find(const datatype& d) {
+		int x=rt;
+		while(x&&t[x].d!=d) x=t[x].son[d>t[x].d];
+		splay(x);
+		return (x ? x : -1);
+	}
+	inline int prev(int p) {
+		if(t[p].son[0]==0) return -1;
+		p=t[p].son[0];
+		while(t[p].son[1]) p=t[p].son[1];
+	}
+	inline int next(int p) {
+		if(t[p].son[1]==0) return -1;
+		p=t[p].son[1];
+		while(t[p].son[0]) p=t[p].son[0];
+	}
+	inline void erase(int x) {
+		if(t[x].cnt==0) return;
+		splay(x);
+		if(t[x].cnt>1) --t[x].cnt;
+		else if(!t[x].son[0]||!t[x].son[1]) {
+			if(t[x].son[0]) rt=t[x].son[0];
+			else rt=t[x].son[1];
+			t[x].clear(),p.push(x);
+			if(t[x].son[0]+t[x].son[1]==0) rt=0;
+		}
+		else {
+			int y=prev(x); splay(y);
+			t[t[x].son[1]].fa=y,t[y].son[1]=t[x].son[1];
+			updnode(y);
+			t[x].clear(),p.push(x);
+		}
+	}
+	//data access : get precursor , get successor
+	inline datatype prec(const datatype& d) {
+		int x=insert(d),y=prev(x);
+		erase(x);
+		if(y==-1) return t[0].d;
+		return t[y].d;
+	}
+	inline datatype succ(const datatype& d) {
+		int x=insert(d),y=next(x);
+		erase(x);
+		if(y==-1) return t[0].d;
+		return t[y].d;
 	}
 };
 
 int main() {
 	return 0;
 }
+
